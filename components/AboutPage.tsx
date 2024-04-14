b331cf7f5d50c833487935f6035debf7ed56c23f
Kevin <vnctkevin@gmail.com>
2024-04-14 21:08:22 +0700

    another update

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -13,11 +13,11 @@ export default function AboutPage() {
         <Navbar />
         <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
           <div className="flex flex-col items-center justify-center gap-4 px-4 text-center md:px-6 md:gap-10">
-            <img src={theme === "dark" ? "logo-kevin-fit-white.png" : "logo-kevin-fit.png"} alt="Kevin" className='w-40' />
+            <img src={theme === "dark" ? "logo-kevin-fit-white.png" : "logo-kevin-fit.png"} alt="Kevin" className='w-40 md:w-52' />
             <div className="space-y-3 md:order-1">
               <div className="space-y-2 text-center md:text-left">
                 
-                <p className="max-w-[600px] text-gray-500 md:text-base/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-200">
+                <p className="max-w-[600px] text-gray-500 md:text-base/relaxed lg:text-base/relaxed xl:text-lg/relaxed dark:text-gray-200">
                 I synthesize ideas into visuals and codes. My name is Kevin. Currently, I'm studying Computer Science at University of Indonesia as an undergraduate student. As a creative and dedicated person who's passionate about <b>visual design, UI/UX design, and software and web development</b>, I have helped people visualize their dreams and visions for more than five years, and I will be more than happy to cooperate with you.
                 </p>
               </div>
